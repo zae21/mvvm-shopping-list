584418431e06b11d738fab52ec7808fd1c45c723
zae21 <prayoga96@gmail.com>
2022-11-03 13:52:12 +0700

    prepare dialog add item

---
+++ app/src/main/java/com/example/mvvmshoppinglist/ui/AddItemDialog.kt
@@ -0,0 +1,8 @@
+package com.example.mvvmshoppinglist.ui
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatDialog
+
+class AddItemDialog(context: Context): AppCompatDialog(context) {
+
+}
